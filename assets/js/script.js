bd677146dc3e6ce120b1989f4694f713b1c998d0
Nic Carrig <cleamcarrig90@hotmail.com>
2022-04-30 03:59:35 -0700

    fixed issue where textarea wasnt being populated

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,11 @@ function loadTasks(){
     //set current day and populate the time-blocks with the descriptions
     //if the date in localstorage is different than current date, wipe the saved descriptions
     var todayDate = moment().format("dddd, MMMM Do");
-    var checkTasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks = JSON.parse(localStorage.getItem("tasks"));
 
-    if(checkTasks === null){
-        tasks[0] = todayDate;
+    if(tasks === null){
+        tasks = [];
+        tasks.push(todayDate);
         saveDesc();
         makeTaskArray();
         loadTasks();
@@ -18,6 +19,13 @@ function loadTasks(){
         loadTasks();
     }
 
+    //populate descriptions from local storage 
+    //starts at 1 because the date is in element [0]
+    for(var i = 1; i < tasks.length; i++){
+        var tempDesc = tasks[i].desc;
+        var tempTime = tasks[i].time;
+        updateDesc(tempDesc, tempTime);
+    }
     $("#currentDay").text(todayDate);
 }
 function saveDesc(){
@@ -29,11 +37,9 @@ function clearDesc(){
     localStorage.clear();
     loadTasks();
 }
-function checkTime(){
-    var currentTime = moment().hour();
-}
 function makeTaskArray(){
-    console.log("make array function");
+    //makes an object array
+
     $.each($(".time-block"), function(){
         var blockId = ($(this).attr("id"));
         var blockDesc = ($(this).find(".description").val());
@@ -48,34 +54,42 @@ function makeTaskArray(){
 
 function updateDesc(text, timeSlot){
     //finds the timeslot of the button clicked and then updates that description
+    var index = findTimeslot(timeSlot);
+    tasks[index].desc = text;
+    console.log(text +" : "+ timeSlot);
+    
+    var idString = "#" + timeSlot + " .description";
+    $(idString).text(text);
 
     saveDesc();
 }
 
-function findTimeslot(){
+function findTimeslot(timeSlot){
     //returns index of button clicked so the correct element in the array can be changed
-    var index;
+    var index = 0;
+    for(var i = 0; i < tasks.length; i++){
+        if(tasks[i].time === timeSlot){
+            index = i;
+        }
+    }
+    
+    return index;
 }
 
 $(".time-block").on("click", "button", function(){
-
+    
     var text = $(this).siblings(".description").val();
     var timeSlot = $(this).parent().attr("id");
-
+    // console.log(text, timeSlot);
     updateDesc(text, timeSlot);
-
+    
 });
 
-// $.each($(".time-block"), function(){
-//     var blockId = ($(this).attr("id"));
-//     var blockDesc = ($(this).find(".description").val());
-//     var blockObj = {
-//         time: blockId,
-//         desc: blockDesc
-//     };
-//     tasks.push(blockObj);
-// })
-// localStorage.setItem("tasks", JSON.stringify(tasks));
+
+function checkTime(){
+    var currentTime = moment().hour();
+}
+
 
 var time = moment().hour();
 console.log(time);
