7798a014ba588a538770e6fced3a065a2cb00d54
Nic Carrig <cleamcarrig90@hotmail.com>
2022-04-29 17:45:47 -0700

    js button functionality

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+//first element is the date, all subsequent elements should be objects with a timeslot and description
+var tasks = [];
+
+function loadTasks(){
+    //set current day and populate the time-blocks with the descriptions
+    //if the date in localstorage is different than current date, wipe the saved descriptions
+    var todayDate = moment().format("dddd, MMMM Do");
+    var savedDesc = localStorage.getItem("tasks");
+    if(savedDesc === null){
+        tasks[0] = todayDate;
+        console.log("null if-else");
+        saveDesc();
+    }
+    else if(todayDate != tasks[0]){
+        //clears all descriptions if the dates are different (presumably from a previous day)
+        console.log("no match if-else");
+        clearDesc();
+    }
+    $("#currentDay").textContent = todayDate;
+    console.log(todayDate);
+    console.log(tasks[0]);
+}
+function saveDesc(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function clearDesc(){
+    //clear storage and recall loadTasks to make date at the top
+    localStorage.clear();
+    loadTasks();
+}
+function checkTime(){
+    var currentTime = moment().hour();
+}
+
+$(".time-block").on("click", "button", function(){
+
+    var text = $(this).siblings(".description").val();
+    console.log(text);
+    var timeSlot = $(this).parent().attr("id");
+    console.log(timeSlot);
+
+});
+
+var time = moment().hour();
+console.log(time);
+loadTasks();
